27e2b8d08396f48e485776f114e1fc59e66ba506
Paulo Cabral Sanz <paulosanz@poli.ufrj.br>
2022-06-16 20:09:44 -0300

    Fix type errors in posix mock

--- src/driver/posix_mock.hpp
+++ src/driver/posix_mock.hpp
@@ -10,20 +10,20 @@ auto randomValue() -> uint16_t {
     std::random_device dev;
     std::mt19937 rng(dev());
     std::uniform_int_distribution<std::mt19937::result_type> dist(1, 1024);
-    return dist(rng);
+    return static_cast<uint16_t>(dist(rng));
 }
 
 auto randomVariation(uint16_t current) -> uint16_t {
-    if (current < 200) return current = std::max(current + randomValue(), 1024);
+    if (current < 200) return current = std::max(std::static_cast<uint16_t>(current + randomValue()), 1024);
 
     std::random_device dev;
     std::mt19937 rng(dev());
     std::uniform_int_distribution<std::mt19937::result_type> dist(-6,0);
-    return std::max(current + dist(rng), 1024);
+    return std::max(std::static_cast<uint16_t>(current + dist(rng)), 1024);
 }
 
 namespace sensor {
-SoilResistivity::SoilResistivity(const iop_hal::PinRaw powerPin) noexcept { this->sensor = new uint16_t(randomValue()); }
+SoilResistivity::SoilResistivity(const iop_hal::PinRaw powerPin) noexcept { (void) powerPin; this->sensor = new uint16_t(randomValue()); }
 
 auto SoilResistivity::begin() noexcept -> void {}
 auto SoilResistivity::measure() noexcept -> uint16_t {
@@ -33,12 +33,12 @@ auto SoilResistivity::measure() noexcept -> uint16_t {
 }
 
 auto SoilResistivity::operator=(SoilResistivity && other) noexcept -> SoilResistivity & {
-    delete this->sensor;
+    delete reinterpret_cast<uint16_t*>(this->sensor);
     this->sensor = other.sensor;
     other.sensor = nullptr;
 }
 SoilResistivity::~SoilResistivity() noexcept {
-    delete this->sensor
+    delete reinterpret_cast<uint16_t*>(this->sensor);
 }
 }
 
